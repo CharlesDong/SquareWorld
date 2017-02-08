2d720d528a69e56b8f6edec6f258382df81f35a6
CharlesDong <2365354490@qq.com>
2017-02-08 10:49:09 +0800

    Update main.cpp
    Delete the comments in front of #include "game.h"

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,27 +1,3 @@
-/*
-Game name: FlatWorld
-Language: C++
-Compiler: Visual Studio 2015
-
-Additional header files:
-<GLFW/glfw3.h>
-<GL/glew.h>
-<SOIL.h>
-<freetype.h>
-GLM header files
-
-Additional LIBs:
-opengl32.lib
-glfw3dll.lib
-glew32.lib
-SOIL.lib
-freetype.lib
-
-Required DLLs:
-glfw3.dll
-glew32.dll
-*/
-
 #include "game.h"
 
 int main()
